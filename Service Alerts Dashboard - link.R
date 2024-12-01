90327b422a787aa777551717b3dcf4ea4ec4cfc5
BrianSok1 <sokbr@bc.edu>
2024-11-30 23:22:45 -0500

    Service Alert
    Added another chart showing the service alerts and the transportation type. (gui_mode_name). Also added my tableau dashboard.

---
+++ Service Alerts Dashboard - link.R
@@ -0,0 +1,9 @@
+
+https://public.tableau.com/app/profile/brian.sok2425/viz/Service_alerts/ServiceAlertsDash?publish=yes
+
+Link to Service Alerts Dashboard
+
+It contains four charts displaying service alert totals for different categories.
+Also explores the Other_causes category and identifies what subcategories fall under Other_causes.
+The three remaining charts show what type of transports these alerts are for and the type of causes.
+This could help us explore what type of alerts will show up in the future.
